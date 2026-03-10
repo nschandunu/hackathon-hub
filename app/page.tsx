2d9c0bc8bd6676ac7dc2a7e5c4f1b71dff351736
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-03-10 14:42:23 +0530

    fix: prevent navbar flash during initial loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,16 @@ import ClientLoader from "@/components/ClientLoader";
 
 export default function Home() {
   return (
-    <>
-      <ClientLoader />
-      
+    <ClientLoader>
       {/* The New Gamified Navbar */}
       <Navbar />
 
       <main className="min-h-screen flex flex-col bg-black text-white selection:bg-white selection:text-black">
-        
         {/* Inject our Animated Landing Page */}
         <div className="flex-1 w-full flex flex-col items-center">
           <LandingPage />
         </div>
       </main>
-    </>
+    </ClientLoader>
   );
 }
